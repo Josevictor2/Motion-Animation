3b2f8a1b5ae8e294f2d9bf9ab741b65167217db5
Josevictor2 <victorcfalx5@hotmail.com>
2023-07-30 23:19:02 -0300

    added new component tabs

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,20 +2,28 @@ import * as React from "react";
 import type { NextPage } from "next";
 
 import { HeadTitle } from "../src/components/Head";
-import { Layout } from "../src/components/Layout";
 
-import { ActiveLink } from "../src/components/LinkActiveNavBar";
-import clsx from "clsx";
+import { Tabs, TabsList, TabsTrigger, TabsContent } from "../src/animation/Tabs";
 
 const Home: NextPage = () => {
   return (
     <>
       <HeadTitle description="Home" />
-      <Layout>
-        <ActiveLink href="/" className="text-red-800">
-          Home
-        </ActiveLink>
-      </Layout>
+      <div className="relative group space-y-2 flex flex-col">
+        <Tabs defaultValue="account" className="w-[400px] relative">
+          <TabsList className='h-16 z-10'>
+              <TabsTrigger value="account" >Account</TabsTrigger>
+              <TabsTrigger value="password" >Password</TabsTrigger>
+              <TabsTrigger value="teste1" >Password</TabsTrigger>
+              <TabsTrigger value="teste2" >Password</TabsTrigger>
+            </TabsList>
+            <div className="absolute w-full border border-gray-600 top-16 z-10" />
+            <TabsContent value="account">Make changes to your account here.</TabsContent>
+            <TabsContent value="password">Change your password here.</TabsContent>
+            <TabsContent value="teste1">Change your password here.</TabsContent>
+            <TabsContent value="teste2">Change your password here.</TabsContent>
+          </Tabs>
+      </div>
     </>
   );
 };
---
+++ src/animation/Tabs/index.tsx
@@ -0,0 +1,57 @@
+"use client"
+
+import * as React from "react"
+import * as TabsPrimitive from "@radix-ui/react-tabs"
+import { cn } from "../../utils/mergeCss"
+
+const Tabs = TabsPrimitive.Root
+
+const TabsList = React.forwardRef<
+  React.ElementRef<typeof TabsPrimitive.List>,
+  React.ComponentPropsWithoutRef<typeof TabsPrimitive.List>
+>(({ className, ...props }, ref) => (
+  <TabsPrimitive.List
+    ref={ref}
+    className={cn(
+      "flex-nowrap whitespace-nowrap no-scrollbar overscroll-x-auto rounded-none border-none bg-transparent m-0 p-0 overflow-x-scroll",
+      className
+    )}
+    {...props}
+  />
+))
+TabsList.displayName = TabsPrimitive.List.displayName
+
+const TabsTrigger = React.forwardRef<
+  React.ElementRef<typeof TabsPrimitive.Trigger>,
+  React.ComponentPropsWithoutRef<typeof TabsPrimitive.Trigger>
+>(({ className, children, ...props }, ref) => (
+  <TabsPrimitive.Trigger
+    ref={ref}
+    className={cn(
+      className="relative rounded-none whitespace-nowrap border-b-2 border-b-transparent bg-transparent px-4 pb-3 pt-2 font-semibold text-gray-100 shadow-none group group-data-[state=active]:shadow-none",
+      className
+    )}
+    {...props}
+    >
+    {children}
+    <div className="absolute h-[8px] w-full group-data-[state=active]:bg-gray-100 mt-2 rounded-t-lg left-0" />
+  </TabsPrimitive.Trigger>
+))
+TabsTrigger.displayName = TabsPrimitive.Trigger.displayName
+
+const TabsContent = React.forwardRef<
+  React.ElementRef<typeof TabsPrimitive.Content>,
+  React.ComponentPropsWithoutRef<typeof TabsPrimitive.Content>
+>(({ className, ...props }, ref) => (
+  <TabsPrimitive.Content
+    ref={ref}
+    className={cn(
+      "mt-2 ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2",
+      className
+    )}
+    {...props}
+  />
+))
+TabsContent.displayName = TabsPrimitive.Content.displayName
+
+export { Tabs, TabsList, TabsTrigger, TabsContent }
